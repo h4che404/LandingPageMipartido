e56ca5a1fba0d3a3b4646daf053feec7e60a7f13
Juan Cruz Elias Martin <108189589+h4che404@users.noreply.github.com>
2025-12-16 13:45:49 -0300

    remove: WaitlistForm and Audience sections from landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,6 @@ import { Hero } from "@/components/sections/hero";
 import { ForCourts } from "@/components/sections/for-courts";
 import { Benefits } from "@/components/sections/benefits";
 import { PainPoints } from "@/components/sections/pain-points";
-import { Audience } from "@/components/sections/audience";
-import { WaitlistForm } from "@/components/features/waitlist-form";
-import { Footer } from "@/components/layout/footer";
 import { PublicBetaList } from "@/components/features/public-beta-list";
 
 export default function Home() {
@@ -14,11 +11,10 @@ export default function Home() {
       <ForCourts />
       <Benefits />
       <PainPoints />
-      <Audience />
-      <WaitlistForm />
       <PublicBetaList />
     </div>
   );
 }
+
 
 
